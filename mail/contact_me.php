8dc5fa10c3411ad9da37014d6f3ea71e1ce86c22
Donald <trung.nguyen@2359media.com>
2021-02-03 16:39:25 +0700

    add mail

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -12,13 +12,25 @@ if(empty($_POST['name'])  		||
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $message = $_POST['message'];
-	
+
+	ini_set("SMTP", "aspmx.l.google.com");
+    ini_set("sendmail_from", "YOURMAIL@gmail.com");
+
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'yensaothanhnha@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
+$headers .= "Reply-To: $email_address";
+
+$email = new \SendGrid\Mail\Mail();
+$email->setFrom($headers);
+$email->setSubject($email_subject);
+$email->addTo($to);
+$email->addContent("text/plain", $email_body);
+);
+$sendgrid = new \SendGrid('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');
+
 mail($to,$email_subject,$email_body,$headers);
 return true;			
 ?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,58 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '36e18d562631b8f829e6b09d25b9c9fcb969fb63',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '36e18d562631b8f829e6b09d25b9c9fcb969fb63',
+    ),
+    'sendgrid/php-http-client' => 
+    array (
+      'pretty_version' => '3.13.0',
+      'version' => '3.13.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '35862113b879274c7014e09681ac279a186665f1',
+    ),
+    'sendgrid/sendgrid' => 
+    array (
+      'pretty_version' => '7.9.2',
+      'version' => '7.9.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ab0023a6233f39e408b5eb8c4299f20790f5f5a7',
+    ),
+    'sendgrid/sendgrid-php' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'starkbank/ecdsa' => 
+    array (
+      'pretty_version' => '0.0.4',
+      'version' => '0.0.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9369d35ed9019321adb4eb9fd3be21357d527c74',
+    ),
+  ),
+);
