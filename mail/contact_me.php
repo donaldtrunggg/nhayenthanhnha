616a2d6e7f622737a434e7647e97d4946a2ab7a4
Donald <trung.nguyen@2359media.com>
2021-02-03 17:59:17 +0700

    add mail

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -17,7 +17,7 @@ $message = $_POST['message'];
 // Create the email and send the message
 $to = 'yensaothanhnha@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Liên hệ mới từ:  $name";
-$email_body = "Bạn nhận được 1 tin nhắn mới từ người dùng website.\n\n"."Tên:\n\nName: $name\n\nEmail: $email_address\n\nNội dung:\n$message";
+$email_body = "Bạn nhận được 1 tin nhắn mới từ người dùng website.\n\n"."\n\nName: $name\n\nEmail: $email_address\n\nNội dung:\n$message";
 
 $email = new \SendGrid\Mail\Mail();
 $email->setFrom("yensaothanhnha@gmail.com", "User from yensaothanhnha.com");
